501392007813edd66835aef47dddaf8c9f393bb8
edoadro <edobecca@gmail.com>
2025-03-01 16:19:57 +0100

    Phase 2
    created MovieAnalysis.py for the class
    created class_test.ipynb for testing
    added project description (.md file)
    updated .gitignore (added datafiles directories, etc.)

---
+++ MovieAnalysis.py
@@ -0,0 +1,61 @@
+import os
+import tarfile
+import urllib.request
+import pandas as pd
+
+class MovieAnalysis:
+    """
+    This class is used to analyze the movie data. It will be used to analyze the data and provide the results to the user.
+    """
+
+    def __init__(self):
+        download_link = 'http://www.cs.cmu.edu/~ark/personas/data/MovieSummaries.tar.gz'
+        download_path = 'MovieSummaries.tar.gz'
+        extract_path = 'MovieData'
+
+        # Check if the directory is empty or doesn't exist
+        if not os.path.exists(extract_path) or not os.listdir(extract_path):
+            self._download_and_extract(download_link, download_path, extract_path)
+
+        # Data and MetaData
+        self.movie_summaries = self._load_data('MovieData/MovieSummaries/plot_summaries.txt', 
+                                               ['Wikipedia movie ID', 'Plot summary'])
+        self.character_data = self._load_data('MovieData/MovieSummaries/character.metadata.tsv', 
+                                              ['Wikipedia movie ID', 'Freebase movie ID', 'Movie release date', 
+                                               'Character name', 'Actor date of birth', 'Actor gender', 'Actor height', 
+                                               'Actor ethnicity', 'Actor name', 'Actor age at movie release', 
+                                               'Freebase character/actor map ID', 'Freebase character ID', 
+                                               'Freebase actor ID'])
+        self.movie_data = self._load_data('MovieData/MovieSummaries/movie.metadata.tsv', 
+                                          ['Wikipedia movie ID', 'Freebase movie ID', 'Movie name', 
+                                           'Movie release date', 'Movie box office revenue', 'Movie runtime', 
+                                           'Movie languages', 'Movie countries', 'Movie genres'])
+        # Test Data
+        self.tvtropes_clusters = self._load_data('MovieData/MovieSummaries/tvtropes.clusters.txt', 
+                                                 ['Character type', 'Freebase character/actor map ID'])
+        self.name_clusters = self._load_data('MovieData/MovieSummaries/name.clusters.txt', 
+                                             ['Character name', 'Freebase character/actor map ID'])
+
+    def _download_and_extract(self, download_link, download_path, extract_path):
+        """Download and extract the tar.gz file."""
+        print('Downloading the file')
+        urllib.request.urlretrieve(download_link, download_path)
+        print('Downloaded the file')
+
+        if not os.path.exists(extract_path):
+            os.makedirs(extract_path)
+
+        with tarfile.open(download_path, 'r:gz') as tar:
+            print('Extracting the file')
+            tar.extractall(path=extract_path)
+            print('Extracted the file')
+
+        os.remove(download_path)
+
+    def _load_data(self, file_path, column_names):
+        """Load data from a file into a pandas DataFrame."""
+        return pd.read_csv(file_path, sep='\t', header=None, names=column_names)
+        
+if __name__ == '__main__':       
+    test = MovieAnalysis()
+    
