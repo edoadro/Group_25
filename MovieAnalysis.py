c7e277bacc6b278f73d0e39dd04766951e79c901
edoadro <edobecca@gmail.com>
2025-03-01 16:57:42 +0100

    Changed MovieData/ to Downloads/

--- MovieAnalysis.py
+++ MovieAnalysis.py
@@ -11,29 +11,29 @@ class MovieAnalysis:
     def __init__(self):
         download_link = 'http://www.cs.cmu.edu/~ark/personas/data/MovieSummaries.tar.gz'
         download_path = 'MovieSummaries.tar.gz'
-        extract_path = 'MovieData'
+        extract_path = 'Downloads'
 
         # Check if the directory is empty or doesn't exist
         if not os.path.exists(extract_path) or not os.listdir(extract_path):
             self._download_and_extract(download_link, download_path, extract_path)
 
         # Data and MetaData
-        self.movie_summaries = self._load_data('MovieData/MovieSummaries/plot_summaries.txt', 
+        self.movie_summaries = self._load_data('Downloads/MovieSummaries/plot_summaries.txt', 
                                                ['Wikipedia movie ID', 'Plot summary'])
-        self.character_data = self._load_data('MovieData/MovieSummaries/character.metadata.tsv', 
+        self.character_data = self._load_data('Downloads/MovieSummaries/character.metadata.tsv', 
                                               ['Wikipedia movie ID', 'Freebase movie ID', 'Movie release date', 
                                                'Character name', 'Actor date of birth', 'Actor gender', 'Actor height', 
                                                'Actor ethnicity', 'Actor name', 'Actor age at movie release', 
                                                'Freebase character/actor map ID', 'Freebase character ID', 
                                                'Freebase actor ID'])
-        self.movie_data = self._load_data('MovieData/MovieSummaries/movie.metadata.tsv', 
+        self.movie_data = self._load_data('Downloads/MovieSummaries/movie.metadata.tsv', 
                                           ['Wikipedia movie ID', 'Freebase movie ID', 'Movie name', 
                                            'Movie release date', 'Movie box office revenue', 'Movie runtime', 
                                            'Movie languages', 'Movie countries', 'Movie genres'])
         # Test Data
-        self.tvtropes_clusters = self._load_data('MovieData/MovieSummaries/tvtropes.clusters.txt', 
+        self.tvtropes_clusters = self._load_data('Downloads/MovieSummaries/tvtropes.clusters.txt', 
                                                  ['Character type', 'Freebase character/actor map ID'])
-        self.name_clusters = self._load_data('MovieData/MovieSummaries/name.clusters.txt', 
+        self.name_clusters = self._load_data('Downloads/MovieSummaries/name.clusters.txt', 
                                              ['Character name', 'Freebase character/actor map ID'])
 
     def _download_and_extract(self, download_link, download_path, extract_path):
@@ -58,7 +58,8 @@ class MovieAnalysis:
     
     def movie_type(self, N: int = 10):
         """
-        comments
+        This function is used to find the top 'N' most common movie types.
+        It returns a DataFrame with the movie type and the number of movies of that type. 
         """
         pass
     
